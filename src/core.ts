ffdf1e07d844afcd14355182f15d38915c9c8dc8
ymizushi <mizushi@gmail.com>
2022-09-06 01:00:25 +0900

    add cron setting

--- src/core.ts
+++ src/core.ts
@@ -1,43 +1,6 @@
 import * as puppeteer from "puppeteer";
-
 import * as sourceMapSupport from "source-map-support";
-
-import fetch from "node-fetch";
-// import * as holiday_jp from "@holiday-jp/holiday_jp"
-
-// const holidays = holiday_jp.between(new Date('2010-09-14'), new Date('2010-09-21'));
-// console.log(holidays[0]['name']); // 敬老の日
-// 
-// const targetYear = 2022
-// const targetMonth = 10 //9月
-
-function dateRange(today: Date, addDays: number): Date[] {
-  return [...new Array(addDays).keys()]
-    .map(num => {
-      const newDate = new Date(today)
-      newDate.setDate(newDate.getDate()+num+1)
-      return newDate
-    })
-}
-
-function splitDateByMonth(days: Date[]): Map<string, Date[]> {
-  const dict = new Map<string, Date[]>() 
-  days.forEach((d: Date) => {
-    const key = (d.getMonth()+1).toString()
-    const value = dict.get(key)
-    if (value !== undefined) {
-      dict.set(key, value.concat([d]))
-    } else {
-      dict.set(key, [])
-    }
-  })
-  return dict
-}
-
-function filterHolidays(dates: Date[]): Date[] {
-  return dates.filter(date => date.getDay() === 0 || date.getDay() === 6)
-}
-
+import { dateRange, filterHolidays, notifySlack, splitDateByMonth } from "./util.js";
 
 const SUCCEEDED = "SUCCEEDED" as const;
 const FAILED = "FAILED" as const;
@@ -50,17 +13,6 @@ export type ActionResult = {
 };
 
 const SLACK_WEBHOOK_URL= process.env.SLACK_WEBHOOK_URL
-
-// Cookie承諾
-async function acceptCookie(page: puppeteer.Page): Promise<ActionResult> {
-  await page.waitForTimeout(3000);
-  await page.click("#onetrust-accept-btn-handler");
-  await page.click('section[class="addcondition"]');
-  return {
-    result: SUCCEEDED,
-    page,
-  };
-}
 
 async function selectDate(
   page: puppeteer.Page,
@@ -106,6 +58,7 @@ async function scrape() {
   const targetMonth = "9" 
   const holidayMap = splitDateByMonth(filterHolidays(dateRange(new Date(), 30)))
 
+  const targetUrl = "https://sea-style-m.yamaha-motor.co.jp"
   const targetMarinas = [
     "[ 横浜 ] D-marina",
     "[ 三浦半島 ] リビエラシーボニアマリーナ",
@@ -114,7 +67,6 @@ async function scrape() {
     "[ 横浜 ] 横浜ベイサイドマリーナ",
     "[ 逗葉 ] 小坪マリーナ",
   ]
-  const targetUrl = "https://sea-style-m.yamaha-motor.co.jp"
   const targetBoats = [
     "ベイフィッシャー",
     "SR-X",
@@ -151,28 +103,33 @@ async function scrape() {
     await page.click('input[type="button"]');
     await page.waitForTimeout(5000)
 
-    const evaluate: (list: Element[]) => Promise<Boat[]> = (async (list: Element[]) => {
-        // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#code-transpilation-issues の理由からeval外のスコープの変数を参照できないのでここで定義
-
-
+    const boats = await page.$$eval("section.contents", async (list: Element[]) => {
         return list.map(element => {
           const marinaPath = element.querySelector("p.marinaName > a")?.getAttribute('href') ?? null
           return {
             boatName: element.querySelector("h2.model")?.textContent ?? null,
             marinaName: element.querySelector("p.marinaName")?.textContent ?? null,
-            marinaUrl: marinaPath ? targetUrl + marinaPath: null,
+            marinaPath: marinaPath ?? null,
           }
         })
-        .filter(e => e.boatName && e.marinaName && e.marinaUrl)
-        .filter(e => e.marinaName && targetMarinas.includes(e.marinaName))
-        .filter(e => e.boatName && targetBoats.filter(b=> e.boatName!.indexOf(b) !==-1).length > 0)
-    })
+        .filter(e => e.boatName && e.marinaName && e.marinaPath)
+    });
 
-    const boats: any = await page.$$eval("section.contents", `${evaluate}`);
-    await notifySlack(boatsStringify(boats, targetYear, targetMonth, targetDate))
+    const filteredBoats = boats
+      .map(e => {
+        return {
+          ...e,
+          marinaUrl: targetUrl + e.marinaPath
+        }
+      })
+      .filter(e => e.marinaName && targetMarinas.includes(e.marinaName))
+      .filter(e => e.boatName && targetBoats.filter(b=> e.boatName!.indexOf(b) !==-1).length > 0)
+    if (filteredBoats.length > 1 && SLACK_WEBHOOK_URL) {
+      await notifySlack(boatsStringify(filteredBoats, targetYear, targetMonth, targetDate), SLACK_WEBHOOK_URL)
+    }
   }
+  browser.close()
 }
-
 
 function boatsStringify(boats: Boat[], targetYear: string, targetMonth: string, targetDate: string): string {
   let line = ""
@@ -187,26 +144,8 @@ function boatsStringify(boats: Boat[], targetYear: string, targetMonth: string, 
   return line
 }
 
-async function notifySlack(content: string) {
-    return await postData(SLACK_WEBHOOK_URL, {
-      text: content,
-    });
-}
-
-async function postData(url = "", data = {}) {
-  return await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-}
-
-function main() {
+(function main() {
   sourceMapSupport.install();
   console.log(`SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL}`);
   scrape();
-}
-
-main();
+})();
---
+++ src/util.ts
@@ -0,0 +1,44 @@
+import fetch from "node-fetch";
+
+export function dateRange(today: Date, addDays: number): Date[] {
+  return [...new Array(addDays).keys()]
+    .map(num => {
+      const newDate = new Date(today)
+      newDate.setDate(newDate.getDate()+num+1)
+      return newDate
+    })
+}
+
+export function splitDateByMonth(days: Date[]): Map<string, Date[]> {
+  const dict = new Map<string, Date[]>() 
+  days.forEach((d: Date) => {
+    const key = (d.getMonth()+1).toString()
+    const value = dict.get(key)
+    if (value !== undefined) {
+      dict.set(key, value.concat([d]))
+    } else {
+      dict.set(key, [])
+    }
+  })
+  return dict
+}
+
+export function filterHolidays(dates: Date[]): Date[] {
+  return dates.filter(date => date.getDay() === 0 || date.getDay() === 6)
+}
+
+export async function notifySlack(content: string, url: string) {
+    return await postData(url, {
+      text: content,
+    });
+}
+
+async function postData(url = "", data = {}) {
+  return await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
